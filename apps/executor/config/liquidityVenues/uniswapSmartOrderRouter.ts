6f01593926d251809d0b419f9d74256d042d1e87
0xshippor <14165372+0xshippor@users.noreply.github.com>
2026-02-05 10:32:28 +0000

    fix: rETH liquidations

--- apps/executor/config/liquidityVenues/uniswapSmartOrderRouter.ts
+++ apps/executor/config/liquidityVenues/uniswapSmartOrderRouter.ts
@@ -23,5 +23,9 @@ export const preferredIntermediateTokens: Record<
     "0xcbB7C0000aB88B473b1f5aFd9ef808440eed33Bf": [
       "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599", // WBTC
     ],
+    // rETH -> try routing through WETH first
+    "0xae78736Cd615f374D3085123A210448E74Fc6393": [
+      "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", // WETH
+    ],
   },
 };
